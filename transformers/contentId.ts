2df2d8d73abe8ecb54819d4c7510e5e3f37775f2
zyhnbyyds <19939926438@163.com>
2025-06-13 17:14:56 +0800

    fix: simplify file path transformation in contentId transformer

--- transformers/contentId.ts
+++ transformers/contentId.ts
@@ -1,11 +1,10 @@
 import { defineTransformer } from '@nuxt/content'
-import { ulid } from 'ulid'
 
 export default defineTransformer({
   name: 'content-id',
   extensions: ['.md'],
   transform(file) {
-    file.path = `/${file.id.split('/').slice(1, 3).join('/')}/${ulid()}`
+    file.path = `/${file.id.split('/').join('/')}`
 
     return {
       ...file,
