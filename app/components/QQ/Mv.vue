3bd86dd140885648343a5ceada0e6269c4f07a33
zyhnbyyds <19939926438@163.com>
2025-07-27 13:12:40 +0800

    refactor(deps): 更新依赖并优化图标样式
    - 更新 @antfu/eslint-config 到 5.0.0
    - 更新 @iconify-json/carbon 到 1.2.11
    - 更新 @iconify-json/material-symbols 到 1.2.30
    - 更新 @nuxtjs/supabase 到 1.6.0
    - 更新 @pinia/nuxt 到 0.11.2
    - 更新 eslint 到 9.32.0
    - 更新 nuxt 到 4.0.1
    - 更新 nuxt-seo-utils 到 7.0.14
    - 更新 vue 到 3.5.18
    - 更新 vue-tsc 到 3.0.4
    
    - 调整 Mv 组件中播放图标的位置和颜色

--- app/components/QQ/Mv.vue
+++ app/components/QQ/Mv.vue
@@ -24,7 +24,7 @@ defineEmits<{
       class="h-full w-full inset-0 object-cover"
     />
     <div class="transition-all left-50% top-50% hover:bg-dark-300 hover:bg-opacity-20 -translate-x-50% -translate-y-50%" p-6 border-2 border-gray-100 rounded-full flex cursor-pointer items-center justify-center absolute @click="$emit('play', props.src, props.videoId)">
-      <Icon name="carbon:play-filled-alt" text-gray-1 text-5 absolute />
+      <Icon name="carbon:play-filled-alt" text-5 text-gray-100 absolute />
     </div>
   </div>
 </template>
